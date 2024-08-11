5ca857bb2faa56fd5bc2eda447c312954b5246f7
Reyshellt <reywilterrero@gmail.com>
2024-08-11 19:12:41 -0400

    Modificacion de prueba y yml

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,14 +1,21 @@
-const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
+const { JSDOM } = require('jsdom');
 
-test('La página tiene un título y un mensaje Hola Mundo', async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(`file://${__dirname}/../index.html`);
-  const title = await page.title();
-  const content = await page.$eval('h1', el => el.textContent);
-
+test('La página tiene un título y un mensaje Hola Mundo', () => {
+  // Cargar el archivo HTML
+  const filePath = path.join(__dirname, '..', 'index.html');
+  const htmlContent = fs.readFileSync(filePath, 'utf-8');
+  
+  // Crear un DOM simulado
+  const dom = new JSDOM(htmlContent);
+  const { document } = dom.window;
+  
+  // Verificar el título de la página
+  const title = document.querySelector('title').textContent;
   expect(title).toBe('Hola Mundo');
-  expect(content).toBe('Hola Mundo');
 
-  await browser.close();
+  // Verificar el contenido de la etiqueta h1
+  const h1Content = document.querySelector('h1').textContent;
+  expect(h1Content).toBe('Hola Mundo');
 });
